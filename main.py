9d0e32dc59e5c34357528c84759d1b31dc3a2a7e
mahsa <mahsamohsenirad1992@gmail.com>
2021-11-11 18:41:43 -0800

    final version

---
+++ main.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("etherium.csv")
+plt.title('ETH price')
+plt.ylabel('Price')
+plt.xlabel('Time')
+plt.grid(True)
+plt.autoscale(axis='x',tight=True)
+data['open'] = data['open'].values[::-1]
+plt.plot(data['open'])
+plt.show()
+data1 = data
+data = data['open'].values.astype(float)
+test_data_size = 12
+train_data = data[:-test_data_size]
+test_data = data[-test_data_size:]
+print(len(train_data))
+print(len(test_data))
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
+train_data_normalized = scaler.fit_transform(train_data .reshape(-1, 1))
+
+train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1)
+train_window = 12
+def create_inout_sequences(input_data, tw):
+    inout_seq = []
+    L = len(input_data)
+    for i in range(L-tw):
+        train_seq = input_data[i:i+tw]
+        train_label = input_data[i+tw:i+tw+1]
+        inout_seq.append((train_seq,train_label))
+    return inout_seq
+train_inout_seq = create_inout_sequences(train_data_normalized, train_window)
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+
+model = LSTM()
+loss_function = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 150
+
+for i in range(epochs):
+    for seq, labels in train_inout_seq:
+        optimizer.zero_grad()
+        model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
+                        torch.zeros(1, 1, model.hidden_layer_size))
+
+        y_pred = model(seq)
+
+        single_loss = loss_function(y_pred, labels)
+        single_loss.backward()
+        optimizer.step()
+
+    if i%25 == 1:
+        print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+
+print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+
+fut_pred = 12
+
+test_inputs = train_data_normalized[-train_window:].tolist()
+print(test_inputs)
+
+model.eval()
+
+for i in range(fut_pred):
+    seq = torch.FloatTensor(test_inputs[-train_window:])
+    with torch.no_grad():
+        model.hidden = (torch.zeros(1, 1, model.hidden_layer_size),
+                        torch.zeros(1, 1, model.hidden_layer_size))
+        test_inputs.append(model(seq).item())
+
+actual_predictions = scaler.inverse_transform(np.array(test_inputs[train_window:] ).reshape(-1, 1))
+print(actual_predictions)
+
+x = np.arange(135, 147, 1)
+print(x)
+
+plt.title('days')
+plt.ylabel('price')
+plt.grid(True)
+plt.autoscale(axis='x', tight=True)
+plt.plot(data1['open'])
+plt.plot(x,actual_predictions)
+plt.show()
